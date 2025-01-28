dc777e89000be384a391212fe0f3091c9aeca5f8
TheRealMrR <richi.is.love@gmail.com>
2025-01-28 18:28:10 -0500

    Update funcionalidad

--- app.js
+++ app.js
@@ -12,17 +12,17 @@ function agregarAmigo(){
         //Restablece a una caja vacia
         document.getElementById('amigo').value = ''; 
         //se llama a la funcion para ser postramada en pantalla
-        amigoHtml(listaAmigos);
+        actualizarLista(listaAmigos);
     } 
 
 }
 
 
-function amigoHtml(lista){
+function actualizarLista(lista){
     let listaHTML = document.getElementById('listaAmigos');
      // inicia la lista en blanco para no tener duplicados
      listaHTML.innerHTML = '';
-    //recorre todo el array y lo agrega a nuestro elemnto html
+    //recorre todo el array y lo agrega a nuestro elemento html
     for (let i = 0; i < lista.length; i++) {
         listaHTML.innerHTML += `<li>${lista[i]}</li>`;
       }
@@ -30,11 +30,14 @@ function amigoHtml(lista){
 }
 
 function sortearAmigo(){
-
-    //Validar que haya amigos disponibles: Antes de sortear, comprobar si el array amigos no está vacío.
-
     let listaSorteados = document.getElementById('resultado');
     let numIndice = Math.floor(Math.random()*listaAmigos.length);
+
+    if (listaAmigos.length === 0) {
+        alert("No hay amigos en la lista. Por favor, agrega amigos antes de sortear.");
+        return;
+    }
+
     if (amigoSorteado.length >= listaAmigos.length) {
         alert("Todos los amigos han sido sorteados.");
         return;
@@ -50,5 +53,3 @@ function sortearAmigo(){
 
 }
 
-   
-    
